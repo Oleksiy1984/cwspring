033f8bea003930038401fbde1eb823a8c61faec3
Oleksiy <o_orlovskiy@ukr.net>
2017-03-12 17:48:42 +0200

    update

--- src/main/java/com/alex/Config/AppConfig.java
+++ src/main/java/com/alex/Config/AppConfig.java
@@ -5,7 +5,6 @@ import com.alex.interfaces.WriteToFile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 import java.text.DateFormat;
 import java.util.Date;
